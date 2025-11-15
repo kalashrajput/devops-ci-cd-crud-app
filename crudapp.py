808e2d417c632abbff00e8ec2fd8031b2f83c42c
Kalash Rajput <kalashrajput>
2025-11-16 03:38:28 +0530

    Fix: added db.create_all to initialize database

--- crudapp.py
+++ crudapp.py
@@ -1,3 +1,9 @@
 from app import app
+from app.models import db
+
+# Create database tables
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
